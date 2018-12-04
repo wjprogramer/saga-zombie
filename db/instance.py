7a1e87a923959af0100433ce86b50f6c2260ddcd
Yz <yanzhen610@gmail.com>
2018-12-05 01:25:12 +0800

    added check_config function

--- db/instance.py
+++ db/instance.py
@@ -1,5 +1,6 @@
-from . import sqlite_db_handler
+
 from utils import eprint
+from . import sqlite_db_handler
 
 def make_db_instance(configs):
     if configs['type'] == 'sqlite':
@@ -9,3 +10,16 @@ def make_db_instance(configs):
         except IOError:
             eprint('could not read or write', configs['path'])
         return sqlite_db_handler.SQLiteDBHandler(configs['path'])
+
+
+def check_db_config(configs):
+    if 'type' not in configs:
+        eprint('database type was not set')
+        exit(1)
+    if configs['type'] == 'sqlite':
+        if 'path' not in configs:
+            eprint('sqlite path was not set')
+            exit(1)
+    else:
+        eprint('unknown database type', configs['type'])
+        exit(1)

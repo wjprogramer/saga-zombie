d4e045c21b1d0018604c7d0a41d9883028d4d020
Yz <yanzhen610@gmail.com>
2018-12-04 17:17:58 +0800

    added docs

--- utils.py
+++ utils.py
@@ -1,4 +1,13 @@
+"""
+utilities module
+"""
+
 import sys
 
+
 def eprint(*args, **kwargs):
+    """
+    print messages to stderr
+    print(*args, file=sys.stderr, **kwargs)
+    """
     print(*args, file=sys.stderr, **kwargs)

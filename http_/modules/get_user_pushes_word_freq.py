43a9ec228a80c14afc13cc477ff7763c2c51d71f
Yz <yanzhen610@gmail.com>
2018-12-12 03:45:22 +0800

    added word frequency statistic

---
+++ http_/modules/get_user_pushes_word_freq.py
@@ -0,0 +1,45 @@
+"""get_user_pushes_word_freq module
+"""
+
+from collections import Counter
+import jieba
+
+from http_.base_module import BaseModule
+from http_.base_module import RequiredParam
+from utils import get_current_time
+
+class Module(BaseModule):
+    """get_user_pushes_word_freq module
+    """
+
+    def required_param(self):
+        return (
+            RequiredParam('username'),
+            RequiredParam('time_begin', int, 604800),
+            RequiredParam('time_end', int, 0)
+        )
+
+    def get_data(self):
+        params = self.get_params()
+        current = get_current_time()
+        username = params[0]
+        time_begin = params[1]
+        time_end = params[2]
+
+        query_result = self.db_handler.query('''
+SELECT `content` FROM `pushes`
+WHERE `author` = (
+    SELECT `id` FROM `users`
+    WHERE `username` = :username
+) AND `date_time` BETWEEN :time_begin AND :time_end ;''',
+            {
+                'username': username,
+                'time_begin': current - time_begin,
+                'time_end': current - time_end
+            })
+
+        counter = Counter()
+        for row in query_result:
+            counter.update(jieba.cut(row[0]))
+
+        return counter
